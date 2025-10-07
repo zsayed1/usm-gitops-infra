36e6ad85ca849bf41e3d22e9550f2471a913516c
Zeshan Sayed <zeshansayed@Zeshans-MacBook-Pro.local>
2025-10-07 18:21:18 -0500

    FMT

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@
 # 1️⃣ VPC – Networking Layer
 #########################################
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  name = "${var.env}-vpc"
-  cidr = var.vpc_cidr
-  azs  = var.azs
+  source = "terraform-aws-modules/vpc/aws"
+  name   = "${var.env}-vpc"
+  cidr   = var.vpc_cidr
+  azs    = var.azs
 
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
@@ -15,7 +15,7 @@ module "vpc" {
 
   enable_dns_hostnames = var.enable_dns_hostnames
   enable_dns_support   = var.enable_dns_support
-  
+
   tags = merge(var.tags, {
     Environment = var.env
     Layer       = "network"
