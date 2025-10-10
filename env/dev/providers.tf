f28262b12b72c8f0ec5225d7a4de63010e6fff43
Zeshan Sayed <zeshansayed@Zeshans-MacBook-Pro.local>
2025-10-09 21:53:22 -0500

    Update terraform module

--- env/dev/providers.tf
+++ env/dev/providers.tf
@@ -2,15 +2,6 @@ provider "aws" {
   region = var.region
 }
 
-# --- EKS Data Sources ---
-data "aws_eks_cluster" "this" {
-  name = "${var.namespace}-${var.env}-eks"
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = "${var.namespace}-${var.env}-eks"
-}
-
 provider "kubernetes" {
   host                   = module.eks_infra.eks_cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks_infra.cluster_certificate_authority_data)
